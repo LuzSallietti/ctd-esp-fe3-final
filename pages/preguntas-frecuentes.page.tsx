e0063afec7b65555a4412db2266a4f72139c386f
Luz Sallietti <luzsallietti@gmail.com>
2024-03-15 21:05:53 -0300

    preguntas-frecuentes added to pages and link added to  Navbar

---
+++ pages/preguntas-frecuentes.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Faqs = () => {
+  return (
+    <div>
+        <h1>Esta es la página de Preguntas Frecuentes</h1>
+      
+    </div>
+  )
+}
+
+export default Faqs
